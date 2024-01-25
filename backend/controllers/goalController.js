ac83fa12a2588467356edc20ed182aafdc0fb9fc
chuanzheng55 <100614122+chuanzheng55@users.noreply.github.com>
2024-01-25 01:12:17 -0500

    CRUD routes set up

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,34 @@
+// @desc: Get goals
+// @route: GET/api/goals
+// @access: private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+};
+
+// @desc: create goals
+// @route: POST/api/goals
+// @access: private
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "set goals" });
+};
+
+// @desc: Update goal
+// @route: PUT/api/goals/:id
+// @access: private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `update goals ${req.params.id}` });
+};
+
+// @desc: Delete goal
+// @route: DELETE/api/goals/:id
+// @access: private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goals ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
